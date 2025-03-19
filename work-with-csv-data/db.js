fd5171089809bbe5291111dffbf1041b64943411
krishoang <kristinker.work@gmail.com>
2025-03-19 15:45:44 +0700

    add script parse csv to array; insert array to sqlite db

---
+++ work-with-csv-data/db.js
@@ -0,0 +1,33 @@
+import sqlite3 from 'sqlite3'
+const sqlite3Verbose = sqlite3.verbose()
+
+const defaultDbFilePath = './db/population.db'
+
+export function connectToDatabase(dbFilePath) {
+  // connect db 
+  const db = new sqlite3Verbose.Database(dbFilePath = defaultDbFilePath)
+  console.log('Connected to the database successfully')
+  return db
+}
+
+export function initDatabase(dbFilePath = defaultDbFilePath) {
+  // init db
+  const db = new sqlite3Verbose.Database(dbFilePath)
+  console.log({ db })
+
+  // init tables
+  db.run(`
+    CREATE TABLE migration
+    (
+      year_month       VARCHAR(10),
+      month_of_release VARCHAR(10),
+      passenger_type   VARCHAR(50),
+      direction        VARCHAR(20),
+      sex              VARCHAR(10),
+      age              VARCHAR(50),
+      estimate         INT
+    )
+  `);
+  console.log('Created database successfully')
+  return db
+}
---
+++ work-with-csv-data/initDatabase.js
@@ -0,0 +1,3 @@
+import { initDatabase } from './db.js'
+
+initDatabase()
